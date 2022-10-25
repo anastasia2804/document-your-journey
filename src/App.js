de0b6ee1653b071e25277e407d312c6c213cbc37
Anastasia Orlova <anastasia2804@gmail.com>
2022-10-24 23:07:51 -0400

    created a heartbeat animation with keyframes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+
+import Homepage from './pages/Homepage';
+import Day1 from './pages/week1/Day1';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/week1/day1" element={<Day1/>}/>
+  
+       
+
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/week1/Day1.js
@@ -0,0 +1,12 @@
+function Day1() {
+    return (
+        <div>
+            <div className="center">
+                <div className="heart"></div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Day1;
